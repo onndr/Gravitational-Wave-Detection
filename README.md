d0aa10387b40a2d20c7c3927d94449e910a49a76
Andrii Gamalii <andrii.gamalii.stud@pw.edu.pl>
2025-01-09 14:02:32 +0100

    minor: refactor

--- README.md
+++ README.md
@@ -18,18 +18,17 @@ Create environment
 
 ```bash
 conda env create -f environment.yml -n gs_env
+conda activate gs_env
 ```
 
 Download dataset
 
 ```bash
-conda activate gs_env
-python download_data.py
+python download_data.py --dataset_path ./data --kaggle_username {name} --kaggle_key {key}
 ```
 
 Train model
 
 ```bash
-conda activate gs_env
 python model.py
 ```
--- download_data.py
+++ download_data.py
@@ -1,6 +1,26 @@
-import kagglehub
+"""Download dataset from Kaggle"""
 
-# Download latest version
-path = kagglehub.dataset_download("tentotheminus9/gravity-spy-gravitational-waves")
+import argparse
+import os
+import kaggle as kg
 
-print("Path to dataset files:", path)
+
+KAGGLE_DATASET = "tentotheminus9/gravity-spy-gravitational-waves"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download Kaggle dataset")
+    parser.add_argument("--dataset_path", required=True, help="Dataset path")
+    parser.add_argument("--kaggle_username", required=True, help="Kaggle username")
+    parser.add_argument("--kaggle_key", required=True, help="Kaggle key")
+
+    args = parser.parse_args()
+
+    os.environ['KAGGLE_USERNAME'] = args.kaggle_username
+    os.environ['KAGGLE_KEY'] = args.kaggle_key
+
+    kg.api.authenticate()
+    kg.api.dataset_download_files(
+        KAGGLE_DATASET,
+        path=args.dataset_path,
+        unzip=True
+    )
